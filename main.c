ddc02c99b4b011fbc96c45fecba03e381ccf656d
heeju1212 <heeju4746@gmail.com>
2023-10-12 15:49:33 +0900

    2번

--- main.c
+++ main.c
@@ -3,20 +3,20 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int printstars(void){
-	int i;
-	for (i=0;i<10;i++){
-		printf("*");
-	}
+void func1(void){
+	int x;
+	printf("func1 x is at %p\n",&x);
 }
 
 
 
 int main(int argc, char *argv[]) {
+	int x; 
+	printf("main x is at %p\n",&x);
+	func1();
 	
-	printstars();
-	printstars();
-	printstars();
+	
+
 	
 	system("PAUSE");
 	return 0;
